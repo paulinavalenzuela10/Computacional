56965a5edfde8b755e489a7ba3d7ededc124420e
paulina_fvc95@hotmail.com <paulinafvc95@gmail.com>
2016-05-01 13:10:59 -0700

    Fin

---
+++ Actividad10/fase.py
@@ -0,0 +1,74 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation as an
+from matplotlib.lines import Line2D
+from scipy.integrate import odeint
+
+#Definiendo las constantes de la Ec. Diferencial
+g = 9.81
+l = 1.0
+b = 0.0 #Pendulo simple por lo tanto sin friccion
+c = g/l
+
+#Condiciones Iniciales
+X_f1 =np.array([(140/180.0)*np.pi,(-140./180.0)*np.pi])
+t = np.linspace(-0.0*np.pi,5.0*np.pi,500) #Para generar la solucion
+
+#Definicion de la ecuacion diderencial del pendulo
+def p (y, t, b, c):
+    theta, omega = y
+    dy_dt = [omega,-b*omega -c*np.sin(theta)]
+    return dy_dt
+
+#=====================================
+# Trayectoria
+y0 = X_f1                               # Punto de Inicio   
+X = odeint(p, y0, t, args=(b,c))         
+
+#=====================================
+#Graficar
+
+class SubplotAnimation(an.TimedAnimation):
+    def __init__(self):
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 1, 1)
+       
+        self.t = np.linspace(0, 80, 400)
+        self.x = X[:,0]
+        self.y = X[:,1]
+
+        self.line1 = Line2D([], [], color='pink')
+        self.line1a = Line2D([], [], color='magenta', linewidth=2)
+        self.line1e = Line2D(
+            [], [], color='magenta', marker='o', markeredgecolor='r')
+        ax1.add_line(self.line1)
+        ax1.add_line(self.line1a)
+        ax1.add_line(self.line1e)
+        ax1.set_xlim(-10, 10)
+        ax1.set_ylim(-10, 10)
+        ax1.set_aspect('equal', 'datalim')
+
+        an.TimedAnimation.__init__(self, fig, interval=50, blit=True)
+
+    def _draw_frame(self, framedata):
+        i = framedata
+        head = i - 1
+        #head_len = 10
+        head_slice = (self.t > self.t[i] - 1.0) & (self.t < self.t[i])
+
+        self.line1.set_data(self.x[:i], self.y[:i])
+        self.line1a.set_data(self.x[head_slice], self.y[head_slice])
+        self.line1e.set_data(self.x[head], self.y[head])
+
+    def new_frame_seq(self):
+        return iter(range(self.t.size))
+
+    def _init_draw(self):
+        lines = [self.line1, self.line1a, self.line1e]
+        for l in lines:
+            l.set_data([], [])
+
+ani = SubplotAnimation()
+#ani.save('test_sub.mp4')
+
+plt.show()
